72e945fadd402f646f8e9df3d5a04f2f50e37a37
Daliska F. Royan <firstaroyan09@gmail.com>
2024-06-09 14:15:24 +0700

    feat: add zsh and alacritty

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-     nix-darwin = {
+    nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -17,8 +17,7 @@
   let
     configuration = { pkgs, ... }: {
       environment.systemPackages =
-        [ pkgs.vim
-        ];
+        [ pkgs.vim ];
 
       services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
@@ -45,6 +44,7 @@
             fonts = [ 
               "FiraCode" 
               "JetBrainsMono" 
+              "Lilex"
             ]; 
           }
         )
@@ -56,6 +56,11 @@
           "google-chrome" 
           "visual-studio-code"
           "obsidian"
+          "postman"
+          "orbstack"
+          "pritunl"
+          "dbeaver-community"
+          "intellij-idea"
         ];
       };
     };
@@ -77,6 +82,160 @@
             init.defaultBranch = "main";
             push.autoSetupRemote = true;
           };
+        };
+        programs.zsh = {
+          enable = true;
+          enableCompletion = true;
+          autosuggestion.enable = true;
+          syntaxHighlighting.enable = true;
+          shellAliases = {
+            ls = "ls --color=auto -F";
+            nixswitch = "darwin-rebuild switch --flake ~/src/system-config/.#";
+            nixup = "pushd ~/src/system-config; nix flake update; nixswitch; popd";
+          };
+          plugins = [
+            {
+              name = "powerlevel10k";
+              src = pkgs.zsh-powerlevel10k;
+              file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+            }
+            {
+              name = "powerlevel10k-config";
+              src = ./.;
+              file = "p10k.zsh";
+            }
+          ];
+          localVariables = {
+            # disable configuration wizard
+			      POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD = true;
+          };
+          # oh-my-zsh = {
+          #   enable = true;
+          #   plugins = [ "git" ];
+          #   custom = "$HOME/.config/zsh_nix/custom";
+          # };
+        };
+        programs.starship.enable = true;
+        programs.starship.enableZshIntegration = true;
+        programs.starship.settings = {
+          character = {
+            success_symbol = "[>](bold green)";
+            error_symbol = "[x](bold red)";
+            vimcmd_symbol = "[<](bold green)";
+          };
+
+          git_commit.tag_symbol = " tag ";
+
+          git_status = {
+            ahead = ">";
+            behind = "<";
+            diverged = "<>";
+            renamed = "r";
+            deleted = "x";
+          };
+
+          aws.symbol = "aws ";
+          azure.symbol = "az ";
+          bun.symbol = "bun ";
+          c.symbol = "C ";
+          cobol.symbol = "cobol ";
+          conda.symbol = "conda ";
+          crystal.symbol = "cr ";
+          cmake.symbol = "cmake ";
+          daml.symbol = "daml ";
+          dart.symbol = "dart ";
+          deno.symbol = "deno ";
+          dotnet.symbol = ".NET ";
+          directory.read_only = " ro";
+          docker_context.symbol = "docker ";
+          elixir.symbol = "exs ";
+          elm.symbol = "elm ";
+          fennel.symbol = "fnl ";
+          fossil_branch.symbol = "fossil ";
+          gcloud.symbol = "gcp ";
+          git_branch.symbol = "git ";
+          golang.symbol = "go ";
+          gradle.symbol = "gradle ";
+          guix_shell.symbol = "guix ";
+          hg_branch.symbol = "hg ";
+          java.symbol = "java ";
+          julia.symbol = "jl ";
+          kotlin.symbol = "kt ";
+          lua.symbol = "lua ";
+          nodejs.symbol = "nodejs ";
+          memory_usage.symbol = "memory ";
+          meson.symbol = "meson ";
+          nim.symbol = "nim ";
+          nix_shell.symbol = "nix ";
+          ocaml.symbol = "ml ";
+          opa.symbol = "opa ";
+          os.symbols = {
+            Alpaquita = "alq ";
+            Alpine = "alp ";
+            Amazon = "amz ";
+            Android = "andr ";
+            Arch = "rch ";
+            Artix = "atx ";
+            CentOS = "cent ";
+            Debian = "deb ";
+            DragonFly = "dfbsd ";
+            Emscripten = "emsc ";
+            EndeavourOS = "ndev ";
+            Fedora = "fed ";
+            FreeBSD = "fbsd ";
+            Garuda = "garu ";
+            Gentoo = "gent ";
+            HardenedBSD = "hbsd ";
+            Illumos = "lum ";
+            Linux = "lnx ";
+            Mabox = "mbox ";
+            Macos = "mac ";
+            Manjaro = "mjo ";
+            Mariner = "mrn ";
+            MidnightBSD = "mid ";
+            Mint = "mint ";
+            NetBSD = "nbsd ";
+            NixOS = "nix ";
+            OpenBSD = "obsd ";
+            OpenCloudOS = "ocos ";
+            openEuler = "oeul ";
+            openSUSE = "osuse ";
+            OracleLinux = "orac ";
+            Pop = "pop ";
+            Raspbian = "rasp ";
+            Redhat = "rhl ";
+            RedHatEnterprise = "rhel ";
+            Redox = "redox ";
+            Solus = "sol ";
+            SUSE = "suse ";
+            Ubuntu = "ubnt ";
+            Unknown = "unk ";
+            Windows = "win ";
+          };
+          package.symbol = "pkg ";
+          perl.symbol = "pl ";
+          php.symbol = "php ";
+          pijul_channel.symbol = "pijul ";
+          pulumi.symbol = "pulumi ";
+          purescript.symbol = "purs ";
+          python.symbol = "py ";
+          raku.symbol = "raku ";
+          ruby.symbol = "rb ";
+          rust.symbol = "rs ";
+          scala.symbol = "scala ";
+          spack.symbol = "spack ";
+          solidity.symbol = "solidity ";
+          status.symbol = "[x](bold red) ";
+          sudo.symbol = "sudo ";
+          swift.symbol = "swift ";
+          typst.symbol = "typst ";
+          terraform.symbol = "terraform ";
+          zig.symbol = "zig ";
+        };
+        programs.alacritty = {
+          enable = true;
+          settings.font.normal.family = "JetBrainsMono Nerd Font";
+          settings.font.size = 12;
         };
     };
   in
