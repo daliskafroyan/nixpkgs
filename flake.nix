ce2dc981013af5925128a0a35fda6fb750f77c5f
Firsta Royan D. <firstaroyan09@gmail.com>
2025-03-24 17:20:40 +0700

    feat: refactor vscode configuration

--- flake.nix
+++ flake.nix
@@ -46,43 +46,7 @@
         # Allow unfree packages in home-manager
         nixpkgs.config.allowUnfree = true;
 
-        programs.vscode = {
-          enable = true;
-          package = pkgs.vscode;
-          profiles.default = {
-            extensions = with pkgs.vscode-extensions; [
-              bbenoist.nix
-            ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-              {
-                name = "theme-cobalt2";
-                publisher = "wesbos";
-                version = "2.5.0";
-                sha256 = "0xvhbzxkpi0yqp8m01dgrakkz6djzzviafh33dvpz9bma85jq8ly";
-              }
-              {
-                name = "monica-code";
-                publisher = "monicaim";
-                version = "1.3.153";
-                sha256 = "1g0ljda8f03qsgsdzha13b1ky0p2mv7mvwg1xn8dhnb97dqpxd1a";
-              }
-            ];
-            userSettings = {
-              "git.autofetch" = true;
-              "git.enableSmartCommit" = true;
-              "git.confirmSync" = false;
-
-              "editor.formatOnSave" = true;
-              "files.autoSave" = "onFocusChange";
-
-              "window.commandCenter" = 1;
-              "editor.fontFamily" = "'JetBrains Mono Nerd Font', monospace";
-              "editor.fontLigatures" = true;
-              "workbench.colorTheme" = "Cobalt2";
-              "workbench.iconTheme" = "vs-seti";
-              "workbench.sideBar.location": "right"
-            };
-          };
-        };
+        programs.vscode = import ./vscode-config.nix { inherit pkgs; };
 
         programs.git = {
           enable = true;
