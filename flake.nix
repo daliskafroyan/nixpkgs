65d8aa08d4bec7b54eb1847ce2faf9fc3a56892d
Firsta Royan D. <firstaroyan09@gmail.com>
2025-03-24 14:19:16 +0700

    feat: tinkering with vscode extensions

--- flake.nix
+++ flake.nix
@@ -42,9 +42,46 @@
       # Then define home-manager configuration
       home-manager.users.yoranium = { pkgs, ... }: {
         home.stateVersion = "23.11";
+        
+        # Allow unfree packages in home-manager
+        nixpkgs.config.allowUnfree = true;
 
         programs.vscode = {
-            enable = true;
+          enable = true;
+          package = pkgs.vscode;
+          profiles.default = {
+            extensions = with pkgs.vscode-extensions; [
+              bbenoist.nix
+            ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+              {
+                name = "theme-cobalt2";
+                publisher = "wesbos";
+                version = "2.5.0";
+                sha256 = "0xvhbzxkpi0yqp8m01dgrakkz6djzzviafh33dvpz9bma85jq8ly";
+              }
+              {
+                name = "monica-code";
+                publisher = "monicaim";
+                version = "1.3.153";
+                sha256 = "1g0ljda8f03qsgsdzha13b1ky0p2mv7mvwg1xn8dhnb97dqpxd1a";
+              }
+            ];
+            userSettings = {
+              "git.autofetch" = true;
+              "git.enableSmartCommit" = true;
+              "git.confirmSync" = false;
+
+              "editor.formatOnSave" = true;
+              "files.autoSave" = "onFocusChange";
+
+              "window.commandCenter" = 1;
+              "editor.fontFamily" = "'JetBrains Mono Nerd Font', monospace";
+              "editor.fontLigatures" = true;
+              "workbench.colorTheme" = "Cobalt2";
+              "workbench.iconTheme" = "vs-seti";
+              "workbench.sideBar.location": "right"
+            };
+          };
         };
 
         programs.git = {
@@ -144,27 +181,6 @@
             };
             # Add more module customizations as desired
           };
-        };
-
-        home.file."Library/Application Support/Cursor/User/settings.json" = {
-          text = builtins.toJSON {
-            "window.commandCenter" = 1;
-            "editor.fontFamily" = "JetBrains Mono, 'Courier New', monospace";
-            "editor.fontLigatures" = true;
-            "workbench.colorTheme" = "Cobalt2";
-            "workbench.iconTheme" = "ayu";
-          };
-        };
-
-        home.file."Library/Application Support/Cursor/User/extensions".source = let
-          extensions = [
-            pkgs.vscode-extensions.bbenoist.nix
-            pkgs.vscode-extensions.catppuccin.catppuccin-vsc
-            pkgs.vscode-extensions.catppuccin.catppuccin-vsc-icons  # Add icons package
-          ];
-        in pkgs.symlinkJoin {
-          name = "cursor-extensions";
-          paths = extensions;
         };
       };
 
